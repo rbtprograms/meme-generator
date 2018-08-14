f480f3d02984317618be0b0d3b379ab014cd1172
rbtprograms <rbt61690@gmail.com>
2018-08-14 16:52:36 -0700

    react is rendering to the dom!

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return(
+      <main>
+        <p>Hello React App</p>
+      </main>
+    )
+  }
+}
+
+export default App;
