6a54f3adfa3bb674e408c70cfbb3bffe6d0c49a3
rbtprograms <rbt61690@gmail.com>
2018-08-15 11:23:51 -0700

    requirements met!

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
 import React, { Component, Fragment } from 'react';
 import styles from './App.css';
+import dom2image from 'dom-to-image';
+import fileSave from 'file-saver';
 
 class App extends Component {
 
   state = { 
     topContent: 'Type your dank meme text',
     bottomContent: 'More dank meme text',
-    url: 'https://s3.amazonaws.com/colorslive/jpg_512x512/3534519-BTjpDbK2uPA0vAO2.jpg'
-    // url: 'https://usatftw.files.wordpress.com/2017/05/spongebob.jpg?w=1000&h=600&crop=1'
+    url: 'https://i0.kym-cdn.com/photos/images/original/000/512/106/5f3.png'
   };
 
   handlePictureChoose = (url = '') => {
@@ -22,6 +23,13 @@ class App extends Component {
     this.setState({ bottomContent });
   };
 
+  handleSave = () => {
+    dom2image.toBlob(this.image)
+      .then(blob => {
+        fileSave.saveAs(blob, 'maymay.png');
+      });
+  };
+
   render() {
     const { url, topContent, bottomContent } = this.state;
 
@@ -34,9 +42,10 @@ class App extends Component {
           <Content label='Bottom' content={bottomContent} onChange={this.handleBottomContentChange}/>
           <Picture url={url} onChoose={this.handlePictureChoose}/>
         </section>
-        <section>
+        <section ref={node => this.image = node}>
           <Meme url={url} topContent={topContent} bottomContent={bottomContent}/>
         </section>
+        <button onClick={this.handleSave}>Save</button>
       </main>
     );
   }
@@ -45,7 +54,7 @@ class App extends Component {
 function Meme({ url, topContent, bottomContent }) {
   return (
     <Fragment>
-      <div  id='container' style={{ background: `url(${url}) no-repeat center/ auto 500px` }}>
+      <div id='container' style={{ background: `url(${url}) no-repeat center/ auto 500px` }}>
         <p id='toptext'>{topContent}</p>
         <p id='bottomtext'>{bottomContent}</p>
       </div>
