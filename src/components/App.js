d68fc91eb1e78ec46bf0bd8e513ed10ced81d948
rbtprograms <rbt61690@gmail.com>
2018-08-15 09:35:00 -0700

    got the top text field rendering

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import styles from './App.css';
 class App extends Component {
 
   state = { 
+    topContent: 'Type your dank meme text',
     url: 'http://i.imgflip.com/18visq.jpg'
   };
 
@@ -12,27 +13,28 @@ class App extends Component {
   };
 
   render() {
-    const { url } = this.state;
+    const { url, topContent } = this.state;
 
     return (
       <main className={styles.app}>
         <h1>Let's make some memes!</h1>
         <section>
           <p id='how'>Upload an image, input your top and bottom text, and save! It's that easy!</p>
+          <Content content={topContent}/>
           <Picture url={url} onChoose={this.handlePictureChoose}/>
         </section>
         <section>
-          <Meme url={url}/>
+          <Meme url={url} topContent={topContent}/>
         </section>
       </main>
     );
   }
 }
 
-function Meme({ url }) {
+function Meme({ url, topContent }) {
   return (
     <Fragment>
-      <pre style={{ background: `url(${url})` }}>What up</pre>
+      <pre style={{ background: `url(${url})` }}>{topContent}</pre>
     </Fragment>
   );
 }
@@ -43,6 +45,19 @@ function Picture({ url, onChoose }) {
       Picture:
       <input value={url} onChange={({ target }) => onChoose(target.value)}/>
     </label>
+  );
+}
+
+function Content({ content }) {
+  return (
+    <p>
+      <label>
+        Top Text Here:
+        <input
+          value={content}
+        />
+      </label>
+    </p>
   );
 }
 
