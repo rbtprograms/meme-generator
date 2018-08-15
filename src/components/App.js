3be8dcaa0d3cf82f253f708d2029c3a817e27e3f
rbtprograms <rbt61690@gmail.com>
2018-08-15 08:42:32 -0700

    got an input with the picture url rendering

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,33 @@ import React, { Component, Fragment } from 'react';
 import styles from './App.css';
 
 class App extends Component {
+
+  state = { 
+    url: 'http://i.imgflip.com/18visq.jpg'
+  }
+
   render() {
+    const { url } = this.state;
+
     return (
       <main className={styles.app}>
-        <h1>Hello React App</h1>
+        <h1>Let's make some memes!</h1>
         <section>
-          <p id='how'>How do you do?</p>
+          <p id='how'>Upload an image, input your top and bottom text, and save! It's that easy!</p>
+          <Picture url={url}/>
         </section>
       </main>
     );
   }
+}
+
+function Picture({ url }) {
+  return (
+    <label>
+      Picture:
+      <input value={url}/>
+    </label>
+  )
 }
 
 export default App;
